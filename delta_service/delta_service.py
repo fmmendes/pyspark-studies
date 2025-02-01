d9da8d5f0d1af99caf29bd86cdc2f8915b095396
Filipe Moraes Mendes <10069692+fmmendes@users.noreply.github.com>
2025-02-01 10:56:43 -0300

    :construction: WIP

--- delta_service/delta_service.py
+++ delta_service/delta_service.py
@@ -2,8 +2,9 @@ import os
 import sys
 from datetime import datetime
 
-from pyspark.sql import SparkSession
 import pyspark.sql.functions as F
+from pyspark.sql import SparkSession
+
 
 class DeltaService:
     def __init__(self):
@@ -13,16 +14,17 @@ class DeltaService:
 
         self.spark = (
             SparkSession.builder
-                .appName("DeltaService")
-                .config(
-                    "spark.jars.packages",
-                        "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.2,"
-                        "io.delta:delta-spark_2.12:3.3.0,"
-                        "io.delta:delta-storage:3.3.0,"
-                        "org.antlr:antlr4-runtime:4.13.2"
-                )
-                .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
-                .getOrCreate()
+            .appName("DeltaService")
+            .config(
+                "spark.jars.packages",
+                "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.2,"
+                "io.delta:delta-spark_2.12:3.3.0,"
+                "io.delta:delta-storage:3.3.0,"
+                "org.antlr:antlr4-runtime:4.13.2"
+            )
+            .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+            .config("spark.sql.debug.maxToStringFields", "1000")  # Adjust the value as needed
+            .getOrCreate()
         )
 
         self.bootstrap_servers = os.environ["BOOTSTRAP_SERVERS"]
@@ -43,11 +45,11 @@ class DeltaService:
     def read_from_kafka(self):
         df = (
             self.spark
-                .read
-                .format("kafka")
-                .option("kafka.bootstrap.servers", self.bootstrap_servers)
-                .option("subscribe", self.topic)
-                .load()
+            .read
+            .format("kafka")
+            .option("kafka.bootstrap.servers", self.bootstrap_servers)
+            .option("subscribe", self.topic)
+            .load()
         )
 
         if df.rdd.isEmpty():
@@ -59,10 +61,10 @@ class DeltaService:
     def infer_schema(self, df):
         df_json = (
             df.withColumn("value", F.expr("string(value)"))
-                .filter((F.col("value").isNotNull()) | (F.col("value") != ""))
-                .select("key", F.expr("struct(offset, value) r"))
-                .groupby("key").agg(F.expr("max(r) r"))
-                .select("r.value")
+            .filter((F.col("value").isNotNull()) | (F.col("value") != ""))
+            .select("key", F.expr("struct(offset, value) r"))
+            .groupby("key").agg(F.expr("max(r) r"))
+            .select("r.value")
         )
 
         df_json.show()
@@ -70,24 +72,68 @@ class DeltaService:
 
         temp_df = self.spark.range(1)
         if df_json.rdd.isEmpty():
+            temp_df = temp_df.withColumn('schema', F.schema_of_json('{}'))
             print("No data found in the Kafka topic.")
-            sys.exit(1)
-
-        temp_df = temp_df.withColumn("schema", F.schema_of_json(df_json.select("value").head()[0]))
+        else:
+            temp_df = temp_df.withColumn("schema", F.schema_of_json(df_json.select("value").head()[0]))
 
         schema = temp_df.select('schema').collect()[0][0]
+
+        print(f'Inferred schema: {str(schema)}')
+
+        before = "before: STRUCT"
+        before_string = "before: STRING"
+        after = "after: STRUCT"
+        after_string = "after: STRING"
+        op_string = "op: STRING"
+
+        if before_string in schema and schema.find(after) > -1:
+            schema = schema.replace(
+                before_string, schema[schema.find(after):schema.find(before_string) - 2].replace(after, before)
+            )
+        elif after_string in schema and schema.find(before) > -1:
+            schema = schema.replace(
+                after_string, schema[schema.find(before):schema.find(op_string) - 2].replace(before, after)
+            )
+
+        schema = (
+            schema.replace("BIGINT", "STRING")
+            .replace("BOOLEAN", "STRING")
+            .replace('BINARY', "STRING")
+            .replace('INT', "STRING")
+            .replace('TIMESTAMP', "STRING")
+        )
+
+        print(f'Final schema: {str(schema)}')
 
         return schema
 
     def process_data(self, df, schema):
-        df = df.selectExpr("CAST(value AS STRING)")
+        df = df.selectExpr("CAST(value AS STRING) as value")
         df = df.withColumn("value", F.from_json(F.col("value"), schema))
         df = df.select(F.col("value.after.*"))
         return df
 
-    def save_to_delta(self, df):
-        # Write data to Delta Lake appending the datetime to the path
-        df.write.format("delta").mode("append").save(f'/bronze/delta/{self.topic}/datetime={datetime.now().strftime("%Y-%m-%d-%H-%M-%S")}')
+    def save_to_delta(self, df, schema):
+        topic_empty_value = 'false' if schema != 'STRUCT<>' else 'true'
+
+        writer = (
+            df
+            .withColumn("value", F.from_json("value", schema))
+            .withColumn("date", F.date_format(F.current_timestamp(), 'yyyy-MM-dd'))
+            .withColumn("topicEmpty", F.lit(topic_empty_value))
+        )
+
+        writer.filter(writer.value.isNotNull()) \
+            .withColumn("value", F.from_json(F.col("value"), schema)) \
+            .select(F.col('value.*'), F.col('date'), F.col('topicEmpty')) \
+            .write \
+            .format('delta') \
+            .partitionBy('date') \
+            .mode('append') \
+            .option("mergeSchema", "true") \
+            .option('checkpointLocation', f'../out/checkpoint_{self.topic}') \
+            .save(f'../out/{self.topic}')
 
     def run(self):
         self.print_spark_parameters()
@@ -97,11 +143,10 @@ class DeltaService:
         df = self.read_from_kafka()
         # Infer schema from the data
         schema = self.infer_schema(df)
-        # TODO Process and normalize the Schema
-
 
         df = self.process_data(df, schema)
-        self.save_to_delta(df)
+        self.save_to_delta(df, schema)
+
 
 if __name__ == "__main__":
     service = DeltaService()
